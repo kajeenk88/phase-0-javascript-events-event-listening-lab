d7fe53cf176bacc9d2ea50e63f2ba0a7f91c8f17
kajeen khalid <khalidkajeen@gmail.com>
2022-07-10 14:14:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-function addingEventListener() {
+function addingEventListener(constinput = document.getElementById('button'))
+
+function clickAlert() {
+  alert(`I was clicked!`)
+}
+
+input.addEventListener('click', clickAlert); {
 }
